778639f5631c1070005a74f338bb47fdc84678d7
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2023-09-28 14:13:49 +0530

    tv link is there

--- nvim/lua/iori/packer.lua
+++ nvim/lua/iori/packer.lua
@@ -22,7 +22,7 @@ vim.cmd.packadd("packer.nvim")
 
 return require("packer").startup(function(use)
     -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+                 use("wbthomason/packer.nvim")
 
     use({
         "nvim-telescope/telescope.nvim",
